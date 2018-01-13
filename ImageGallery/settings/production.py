a6a30031a9499f4b58327a35d7ea03faf613922f
Dileep <dileepmeena57@gmail.com>
2018-01-13 22:38:10 +0530

    change in production

--- ImageGallery/settings/production.py
+++ ImageGallery/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'gv8m4tppnq!%hn*jna9)e90tt*r2x9)_e35yr8g+13i()2rtn5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','https://imageworld.herokuapp.com/','imageworld.herokuapp.com']
 
